81bc3ae256d32ecd34fb3c4976f84737596d81f6
DhruvDaherawala <123897835+DhruvDaherawala@users.noreply.github.com>
2024-09-13 11:57:10 +0000

    Simple array Added

---
+++ Arr_print_1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+
+    int arr[] = {1,2,3,4,5};
+    printf("\n");
+    for(int i = 0; i < 5;i++){
+        printf("array no %d is %d\n\n",1+i,arr[i]);
+    }
+    return 0;
+}
